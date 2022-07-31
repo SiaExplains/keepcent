eb2e53900ac6277619846c8f5e8e05cad81a5ac2
Siavash Ghanbari <sia.qnbr@gmail.com>
2022-07-31 14:45:49 +0200

    ET-1000 Change html attributes to react attributes

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 export const IncomeExpenses = () => {
   return (
-    <div class="inc-exp-container">
-        <div>
+    <div className="inc-exp-container">
+      <div>
         <h4>Income</h4>
         <p className="money plus">+$0.00</p>
-        </div>
-        <div>
+      </div>
+      <div>
         <h4>Expense</h4>
         <p className="money minus">-$0.00</p>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
