550319a30d117bd4c4a3d340c906491f954f2831
Siavash Ghanbari <sia.qnbr@gmail.com>
2022-07-31 14:42:13 +0200

    ET-1000 Create necessary components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { AddTransaction } from './components/AddTransaction';
+import { Balance } from './components/Balance';
+import Header from './components/Header';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
 
 function App() {
   return (
     <div className="App">
-      Expense Tracker
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
