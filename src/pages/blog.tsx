7734ab7013e7ca23044c8c1ad8ba4ead03b8643c
Sheida Mohassesy <sheida.mohassesi@gmail.com>
2022-09-02 15:43:15 +0200

    KC-1002 Create a Blog Page

---
+++ src/pages/blog.tsx
@@ -0,0 +1,11 @@
+const BlogPage = () => {
+  return (
+    <div>
+      <h1>Welcome to Blog!</h1>
+      <br />
+      <div>We will complete this page soon!</div>
+    </div>
+  );
+};
+
+export default BlogPage;
