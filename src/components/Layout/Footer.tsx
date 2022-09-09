a023b7fa0f2fce20783c67bc5f2671054822f77e
Sheida Mohassesy <sheida.mohassesi@gmail.com>
2022-09-09 18:48:01 +0200

    KC-1004 Enrich footer with elements

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -4,12 +4,13 @@ import ColoredLogo, { LogoSizes } from '../ColoredLogo';
 const Footer = () => {
   return (
     <div className="et-footer">
-      <div className="copyright">
-        {/* <img src="/white-logo.png" /> */}
-        <ColoredLogo size={LogoSizes.Large} color="#476b8a" bkcolor="white" />
-        <div className="copyright-text">
-          Copyrights &copy; 2022 <br />
-          All rights reserved.
+      <div className="copyright-container">
+        <div className="copyright-item">
+          <ColoredLogo size={LogoSizes.Large} color="#476b8a" bkcolor="white" />
+          <div className="copyright-text">
+            Copyrights &copy; 2022 <br />
+            All rights reserved.
+          </div>
         </div>
       </div>
 
@@ -27,8 +28,36 @@ const Footer = () => {
         <Link to="/sitemap">SiteMap</Link>
         <Link to="/login">Login</Link>
       </div>
-      <div className="social"></div>
-      <div className="contact"></div>
+      <div className="social">
+        <p>
+          KeepCent is a simple playground app for developing a personal budget application. KeepCent
+          will help us to track all expenses and make a history of them.
+        </p>
+        <div className="social-logos">
+          <img src="/socials/facebook.png" />
+          <img src="/socials/instagram.png" />
+          <a href="https://twitter.com/keepcent" target="_blank">
+            <img src="/socials/twitter.png" />
+          </a>
+          <img src="/socials/youtube.png" />
+        </div>
+      </div>
+      <div className="contact-container">
+        <div className="contact-item">
+          <div>
+            <i className="fa fa-mobile" />
+            &nbsp;<a href="tel:+123-456-910">+123-456-910</a>
+          </div>
+          <div>
+            <i className="fa fa-phone" />
+            &nbsp;<a href="tel:+123-456-910">+123-456-910</a>
+          </div>
+          <div>
+            <i className="fa fa-envelope" />
+            &nbsp;<a href="mailto:keepcent@gmail.com">keepcent@gmail.com</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
