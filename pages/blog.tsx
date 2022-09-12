cab7f932c42b9c2a07f1c3455f37249983258725
Sheida Mohassesy <sheida.mohassesi@gmail.com>
2022-09-12 12:37:45 +0200

    KC-1009 Enrich blog page

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Layout from '../src/components/Layout/Layout';
 
 const BlogPage = () => {
@@ -5,8 +6,54 @@ const BlogPage = () => {
     <Layout>
       <div className="container">
         <h1>Welcome to Blog!</h1>
-        <br />
-        <div>We will complete this page soon!</div>
+        <div className="blog-container">
+          <h2>The first post</h2>
+          <div className="blog-date">9/12/2022</div>
+          <div>
+            <p>
+              In this post we introduce KeepCent. KeepCent is a simple playground app for developing
+              a personal budget application...
+            </p>
+            <div className="tags">
+              <span className="tag">App</span>
+            </div>
+            <Link href={'#'}>
+              <a className="read-more">Read More</a>
+            </Link>
+          </div>
+        </div>
+        <div className="blog-container">
+          <h2>The second post</h2>
+          <div className="blog-date">9/12/2022</div>
+          <div>
+            <p>
+              In this post we introduce KeepCent. KeepCent is a simple playground app for developing
+              a personal budget application...
+            </p>
+            <div className="tags">
+              <span className="tag">App</span>
+            </div>
+            <Link href={'#'}>
+              <a className="read-more">Read More</a>
+            </Link>
+          </div>
+        </div>
+        <div className="blog-container">
+          <h2>The third post</h2>
+          <div className="blog-date">9/12/2022</div>
+          <div>
+            <p>
+              In this post we introduce KeepCent. KeepCent is a simple playground app for developing
+              a personal budget application...
+            </p>
+            <div className="tags">
+              <span className="tag">App</span>
+            </div>
+            <Link href={'#'}>
+              <a className="read-more">Read More</a>
+            </Link>
+          </div>
+        </div>
       </div>
     </Layout>
   );
